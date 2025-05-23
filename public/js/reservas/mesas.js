feccc24451e8d198692c41e9e68701c637d7d638
Abdieeel <marcelopostigo54@gmail.com>
2025-05-23 11:56:34 -0500

    work(reservas): trabajando en seccion personas

--- public/js/reservas/mesas.js
+++ public/js/reservas/mesas.js
@@ -1,23 +1,30 @@
 const btnSalaPrincipal = document.getElementById("btn-sala-principal");
 const btnSalaExterior = document.getElementById("btn-sala-exterior");
 const contendorMesas = document.getElementById("contenedor-mesas");
+const btnNav = document.getElementById("btn-mesa");
 
 const mesasPrincipal = [
-    {nombre: "Mesa 01", disponible: true },
-    {nombre: "Mesa 02", disponible: false },
-    {nombre: "Mesa 03", disponible: false },
-    {nombre: "Mesa 04", disponible: true },
-    {nombre: "Mesa 05", disponible: true },
-    {nombre: "Mesa 06", disponible: true }
+    { nombre: "Mesa 01", disponible: true },
+    { nombre: "Mesa 02", disponible: false },
+    { nombre: "Mesa 03", disponible: false },
+    { nombre: "Mesa 04", disponible: true },
+    { nombre: "Mesa 05", disponible: true },
+    { nombre: "Mesa 06", disponible: true }
 ];
 
 const mesasExterior = [
-    {nombre: "Mesa 07", disponible: true },
-    {nombre: "Mesa 08", disponible: true },
-    {nombre: "Mesa 09", disponible: false }
+    { nombre: "Mesa 07", disponible: true },
+    { nombre: "Mesa 08", disponible: true },
+    { nombre: "Mesa 09", disponible: false }
 ];
 
-function renderizarMesas(mesas){
+btnNav.addEventListener('click', () => {
+    btnSalaExterior.classList.remove('activo');
+    btnSalaPrincipal.classList.add('activo');
+    renderizarMesas(mesasPrincipal);
+});
+
+function renderizarMesas(mesas) {
     contendorMesas.innerHTML = '';
     mesas.forEach(mesa => {
         const btn = document.createElement('button');
@@ -28,18 +35,14 @@ function renderizarMesas(mesas){
 }
 
 btnSalaPrincipal.addEventListener('click', () => {
-    btnSalaPrincipal.classList.add('activo');
-    btnSalaPrincipal.classList.remove('no-active');
     btnSalaExterior.classList.remove('activo');
-    btnSalaExterior.classList.add('no-active');
+    btnSalaPrincipal.classList.add('activo');
     renderizarMesas(mesasPrincipal);
 });
 
 btnSalaExterior.addEventListener('click', () => {
     btnSalaExterior.classList.add('activo');
-    btnSalaExterior.classList.remove('no-active');
     btnSalaPrincipal.classList.remove('activo');
-    btnSalaPrincipal.classList.add('no-active');
     renderizarMesas(mesasExterior);
 });
 
